fbdc7f2f5e4801deab95a04bc8b70f77d24aa8cf
Reini Urban <rurban@x-ray.at>
2010-07-29 18:59:57 +0000

    more test updates as time passes...
    git-svn-id: http://perl-compiler.googlecode.com/svn/trunk@474 ed534f1a-1453-0410-ab30-dfc593a8b23c

--- t/test.pl
+++ t/test.pl
@@ -541,8 +541,7 @@ sub todo_tests_default {
         # 5.6.2 CORE: 8,15,16,22. 16 fixed with 1.04_24, 8 with 1.04_25
         # 5.8.8 CORE: 11,14,15,20,23 / non-threaded: 5,7-12,14-20,22-23,25
         @todo = (15,41..44)   if $] < 5.007;
-        # on cygwin 29 passes
-        @todo = (29,39,41,44)  if $] >= 5.010;
+        @todo = (39,41,44)  if $] >= 5.010;
         @todo = (15,29,39,44)  if $] >= 5.010 and !$ITHREADS;
         push @todo, (27)       if $] >= 5.012 and $ITHREADS;
         push @todo, (6,8..10,16,21,24,26,30,31,35) if $] >= 5.013002; #CV broken
@@ -561,7 +560,7 @@ sub todo_tests_default {
         push @todo, (12) if $^O eq 'MSWin32' and $Config{cc} =~ /^cl/i;
         push @todo, (44);
         push @todo, (3,4,27,42,43) if $] >= 5.011004 and $ITHREADS;
-        push @todo, (103) if $] >= 5.013002;
+        push @todo, (15,103) if $] >= 5.012001;
         push @todo, (6,8..10,16,21,24,26,30,31,35) if $] >= 5.013002; #CV broken
 
         push @todo, (26) if $what =~ /^cc_o[12]/;
