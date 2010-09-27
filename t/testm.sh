d7d2b1cbac75e6bb601e2f3e779a9cdf2e08a151
Reini Urban <rurban@x-ray.at>
2010-09-27 11:29:02 +0000

    permission denied to execute on Win7 for ExtUtils::Install
    git-svn-id: http://perl-compiler.googlecode.com/svn/trunk@641 ed534f1a-1453-0410-ab30-dfc593a8b23c

--- t/testm.sh
+++ t/testm.sh
@@ -84,7 +84,7 @@ if [ -n "$1" ]; then
     else
 	while [ -n "$1" ]; do
 	    # single module
-	    name="$(perl -e'$ARGV[0]=~s{::}{_}g; print lc($ARGV[0])' $1)"
+	    name="$(perl -e'$a=shift;$a=~s{::}{_}g;$a=~s{install}{inst}i;print lc($a)' $1)"
 	    if [ "${KEEP:0:2}" = "-D" ]; then
 	      echo $PERL $Mblib -MO=C,$KEEP,-o$name.c -e "\"use $1; print 'ok'\""
 	      $PERL $Mblib -MO=C,$KEEP,-o$name.c -e "use $1; print 'ok'"
