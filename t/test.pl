4980a9b9b4124fe2ca6e28604386d779dabd9d02
Reini Urban <rurban@x-ray.at>
2010-10-14 16:48:57 +0000

    status with active confess REGEXP PMOP (more fails). revert it. update test status
    git-svn-id: http://perl-compiler.googlecode.com/svn/trunk@741 ed534f1a-1453-0410-ab30-dfc593a8b23c

--- t/test.pl
+++ t/test.pl
@@ -547,17 +547,13 @@ sub todo_tests_default {
         @todo = (15,35,39,44,46)    if $] < 5.010;
         @todo = (15,35,41..46,103)  if $] < 5.007;
         push @todo, (41..43,103)    if $] >= 5.010;
-        push @todo, (39,41)
-	  if $what =~ 'c_o2' and $DEBUGGING;
-        push @todo, (39)
-	  if $what =~ 'c_o2' and $DEBUGGING and !$ITHREADS;
-        push @todo, (11)
-	  if $what =~ 'c_o2' and $DEBUGGING and $ITHREADS;
+        push @todo, (17..20,22,34..41)
+	  if $what =~ 'c_o2';
         #push @todo, (26) if $what eq 'c_o1' and $] < 5.010;
 	push @todo, (10,12)
 	  if $what =~ /c_o[234]/ and $] >= 5.010;
 	push @todo, (11,28)
-	  if $what =~ /c_o[1234]/;
+	  if $what =~ /c_o[234]/;
 	push @todo, (25)
 	  if $what eq 'c_o' and $^O eq 'MSWin32';
 	push @todo, (19,25)
@@ -565,9 +561,9 @@ sub todo_tests_default {
     } elsif ($what =~ /^cc/) {
         # 8,11,14..16,18..19 fail on 5.00505 + 5.6, old core failures (max 20)
         # on cygwin 29 passes
-        @todo = (14,15,21,30,35,44,103); #5.8.9
+        @todo = (14,15,21,28,30,35,39,44,103); #5.8.9
         push @todo, (27,41..46) if $] < 5.007;
-        push @todo, (25,41..43) if $] >= 5.010;
+        push @todo, (29,41..44) if $] >= 5.010;
         # solaris also. I suspected nvx<=>cop_seq_*
         push @todo, (12) if $^O eq 'MSWin32' and $Config{cc} =~ /^cl/i;
         push @todo, (3,4,27,42,43) if $] >= 5.011004 and $ITHREADS;
