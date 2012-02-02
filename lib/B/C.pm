rurban/perl-compiler

commit fd3317390d828716b18f71d18f6362ffe2d63f9f

More heuristics when method package_pv not found
Special case Carp calling threads and B methods.
Sanified package_pv default a bit when a method padsv was not found:
  method_name only passes through the default and keeps it, so
  set the default to the new package if it actually was found.
Fix -u<module> without file extension.

lib/B/C.pm
@@ -1091,15 +1091,20 @@ sub method_named {
     if (defined(&$method)) {
       warn sprintf( "Found &%s::%s\n", $_, $name ) if $debug{cv};
       $include_package{$_} = 1; # issue59
+      $package_pv = $_;
       mark_package($_, 1);
       last;
     } else {
+      return if $method =~ /^threads::(GV|NAME|STASH)$/; # Carp artefact to ignore B
+      return if $method eq 'threads::tid' and !$ITHREADS; # Without ithreads threads.pm is not loaded
       if (my $parent = try_isa($_,$name)) {
 	warn sprintf( "Found &%s::%s\n", $parent, $name ) if $debug{cv};
 	$method = $parent . '::' . $name;
 	$include_package{$parent} = 1;
+	$package_pv = $parent;
 	last;
       }
+      $method = $package_pv.'::'.$name;
       warn "no definition for method_name \"$method\"\n" if $debug{cv};
     }
   }
@@ -5612,7 +5617,11 @@ OPTION:
     }
     elsif ( $opt eq "u" ) {
       $arg ||= shift @options;
-      require $arg;
+      if ($arg =~ /\.p/) {
+	eval "require $arg;";
+      } else {
+	eval "require $arg.pm;";
+      }
       mark_unused( $arg, 1 );
     }
     elsif ( $opt eq "U" ) {

commit 04c3094ec376cb789e3e8201a00f08d20b7cdd95

revise prev. change which broke 50
fd3317390 broke test 50 because an undefined CV was saved.
Break out earlier.

lib/B/C.pm
@@ -1089,28 +1089,26 @@ sub method_named {
     no strict 'refs';
     $method = $_ . '::' . $name;
     if (defined(&$method)) {
-      warn sprintf( "Found &%s::%s\n", $_, $name ) if $debug{cv};
       $include_package{$_} = 1; # issue59
       $package_pv = $_;
       mark_package($_, 1);
-      last;
+      warn "save found method_name \"$method\"\n" if $debug{cv};
+      return svref_2object( \&{$method} );
     } else {
       return if $method =~ /^threads::(GV|NAME|STASH)$/; # Carp artefact to ignore B
       return if $method eq 'threads::tid' and !$ITHREADS; # Without ithreads threads.pm is not loaded
       if (my $parent = try_isa($_,$name)) {
-	warn sprintf( "Found &%s::%s\n", $parent, $name ) if $debug{cv};
 	$method = $parent . '::' . $name;
 	$include_package{$parent} = 1;
 	$package_pv = $parent;
-	last;
+	warn "save found method_name \"$method\"\n" if $debug{cv};
+	return svref_2object( \&{$method} );
       }
-      $method = $package_pv.'::'.$name;
-      warn "no definition for method_name \"$method\"\n" if $debug{cv};
     }
   }
-  $method = $name unless $method;
-  warn "save method_name \"$method\"\n" if $debug{cv};
-  return svref_2object( \&{$method} );
+  $method = $package_pv.'::'.$name;
+  warn "no definition for method_name \"$method\"\n" if $debug{cv};
+  return;
 }
 
 sub B::SVOP::save {
@@ -2670,7 +2668,7 @@ sub B::CV::save {
 	  my $gv = $cv->GV;
 	  if ($$gv) {
 	    if ($cvstashname ne $gv->STASH->NAME or $cvname ne $gv->NAME) { # UNIVERSAL or AUTOLOAD
-	      warn "New ".$gv->STASH->NAME."::".$gv->NAME." autoloaded\n" if $debug{sub};
+	      warn "New ".$gv->STASH->NAME."::".$gv->NAME." autoloaded. remove old cv\n" if $debug{sub};
 	      $svsect->remove;
 	      $xpvcvsect->remove;
 	      delsym($cv);
@@ -2687,7 +2685,7 @@ sub B::CV::save {
 	my $gv = $cv->GV;
 	if ($$gv) {
 	  if ($cvstashname ne $gv->STASH->NAME or $cvname ne $gv->NAME) { # UNIVERSAL or AUTOLOAD
-	    warn "Recalculated root and xsub $gv->STASH->NAME\::$gv->NAME\n" if $verbose;
+	    warn "Recalculated root and xsub $gv->STASH->NAME\::$gv->NAME. remove old cv\n" if $verbose;
 	    $svsect->remove;
 	    $xpvcvsect->remove;
 	    delsym($cv);
@@ -2754,14 +2752,14 @@ sub B::CV::save {
   }
   else {
     warn "&".$fullname." not found\n" if $verbose or $debug{sub};
-    warn "No definition for sub $fullname (unable to autoload)\n"
+    warn "No definition for sub $fullname (unable to autoload), remove cv\n"
       if $debug{cv};
     $init->add( "/* $fullname not found */" ) if $verbose or $debug{sub};
     # XXX empty CV should not be saved
-    # $svsect->remove( $sv_ix );
-    # $xpvcvsect->remove( $xpvcv_ix );
-    # delsym( $cv );
-    # return '0';
+    $svsect->remove( $sv_ix );
+    $xpvcvsect->remove( $xpvcv_ix );
+    delsym( $cv );
+    return '0';
   }
 
   # Now it is time to record the CV

script/perlcc.PL
@@ -52,6 +52,7 @@ print OUT <<'!NO!SUBS!';
 # Version 2.11, Reini Urban, 2011-04-11 20:16:00
 # Version 2.12, Reini Urban, 2011-10-02 05:19:00
 # Version 2.13, Reini Urban, 2012-01-10 13:03:00
+# Version 2.14, Reini Urban, 2012-02-01 20:00:00
 
 use strict;
 use warnings;
@@ -62,11 +63,11 @@ use Config;
 use Fcntl qw(:DEFAULT :flock);
 use File::Temp qw(tempfile);
 use File::Basename qw(basename dirname);
-use Cwd;
+# use Cwd;
 use Pod::Usage;
 # Time::HiRes does not work with 5.6
 # use Time::HiRes qw(gettimeofday tv_interval);
-our $VERSION = 2.13;
+our $VERSION = 2.14;
 $| = 1;
 eval { require B::C::Flags; };
 
@@ -764,7 +765,7 @@ sub check_write {
     if (-e _) {
         _die("Cannot write on $file: $!\n") unless -w _;
     }
-    unless (-w cwd()) {
+    unless (-w '.') {
         _die("Cannot write in this directory: $!\n");
     }
 }