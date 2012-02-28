eec7b38081e5e121f2c334fd7137329735b19ca7
Reini Urban <rurban@cpanel.net>
2012-02-28 13:21:07 -0600

    add DateTime to critical.sh, move diff after

--- t/critical.sh
+++ t/critical.sh
@@ -6,11 +6,12 @@ test -f $log && mv $log $log~
 tests="15 27 29 511 224 227 72 74 91 95"
 echo $p > $log
 git log --oneline -1 >> $log
-git diff >> $log
 
 t/testc.sh -q -O0 $tests 2>&1 | tee -a $log
 t/testc.sh -q -O3 $tests 2>&1 | tee -a $log
 $p -Iblib/arch -Iblib/lib t/perldoc.t 2>&1 | tee -a $log
+t/testm.sh DateTime 2>&1 | tee -a $log
 
 git log --oneline -1 >> $log
+git diff >> $log
 $p -V >> $log
