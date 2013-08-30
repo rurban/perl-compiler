59026624a97204256b35b8cfa0c29873f1b07a5b
Reini Urban <rurban@cpanel.net>
2013-08-30 12:41:18 -0500

    t/critical.sh: echo perl and last commit

--- t/critical.sh
+++ t/critical.sh
@@ -4,8 +4,8 @@ log=log.critical-`basename $p`-g`git rev-parse HEAD|cut -c1-8`
 test -f $log && mv $log $log~
 # 91 = issue 59
 tests="15 27 29 511 224 227 72 74 91 95"
-echo $p > $log
-git log --oneline -1 >> $log
+echo $p | tee $log
+git log --oneline -1 | tee $log
 
 t/testc.sh -q -O0 $tests 2>&1 | tee -a $log
 t/testc.sh -q -O3 $tests 2>&1 | tee -a $log
