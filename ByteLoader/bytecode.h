7a95cab174e233d41ad97b2efd732f2b8356282d
Reini Urban <rurban@cpanel.net>
2013-12-31 10:06:28 -0600

    BC: 5.19.3 AvPAD_NAMELIST for eval string
    set PadnamelistMAXNAMED() analog to 363f48e708af96

--- ByteLoader/bytecode.h
+++ ByteLoader/bytecode.h
@@ -784,7 +784,13 @@ static int bget_swab = 0;
 	    padl = (SV*)pad_new(arg);			\
 	    BSET_OBJ_STOREX(padl);			\
 	} STMT_END
+#if (PERL_VERSION >= 19) || ( PERL_VERSION == 19 && PERL_SUBVERSION > 3)
+#define BSET_padl_name(padl, pad)                \
+    PadlistARRAY((PADLIST*)padl)[0] = (PAD*)pad; \
+    PadnamelistMAXNAMED((PAD*)pad) = AvFILL((AV*)pad)
+#else
 #define BSET_padl_name(padl, pad)  PadlistARRAY((PADLIST*)padl)[0] = (PAD*)pad
+#endif
 #define BSET_padl_sym(padl, pad)   PadlistARRAY((PADLIST*)padl)[1] = (PAD*)pad
 #define BSET_xcv_name_hek(cv, arg)                                      \
   STMT_START {                                                          \
