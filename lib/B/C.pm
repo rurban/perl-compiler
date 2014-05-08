026c297612ded37eefd45b13f49721063bdbce34
Reini Urban <rurban@cpanel.net>
2014-05-08 08:26:42 -0500

    C: protect from empty $fullname arg to B::PVMG::save
    detected and fixed by Todd Rinaldo, fixes #321

--- lib/B/C.pm
+++ lib/B/C.pm
@@ -2530,7 +2530,8 @@ sub B::PVMG::save {
     # Detect ptr to extern symbol in shared library and remap it in init2
     # Safe and mandatory currently only Net-DNS-0.67 - 0.74.
     # svop const or pad OBJECT,IOK
-    if ($fullname =~ /^svop const|^padop|^Encode::Encoding| :pad\[1\]/ 
+    if ($fullname
+        and $fullname =~ /^svop const|^padop|^Encode::Encoding| :pad\[1\]/
         and $ivx =~ /U?L+$/
         and ref($sv->SvSTASH) ne 'B::SPECIAL')
     {
