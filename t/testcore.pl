15cfb7aaf44613c33154366a45cc43221b3ccb46
Reini Urban <rurban@cpanel.net>
2014-08-27 17:18:47 -0500

    fix t/CORE/comp/retainedlines.t for 5.20

--- t/testcore.pl
+++ t/testcore.pl
@@ -70,10 +70,10 @@ my $SKIP =
 # for C only, tested with 5.21.3d-nt
 my @fail = map { "t/CORE/$_" }
   (#'comp/colon.t', # ok with 5.14, 5.18, failed since 5.20 (fixed with #372)
-   'comp/hints.t',
-   'comp/packagev.t',
+   #'comp/hints.t',
+   #'comp/package.t', # fails only with -O0
    #'comp/parser.t',# ok with 5.14, failed since 5.18, updated with 1.51_02
-   'comp/retainedlines.t',# ok with 5.14, fails since 5.18
+   #'comp/retainedlines.t',# ok with 5.14, failed since 5.18. fixed test
    'comp/script.t',
    'io/layers.t',
    'op/array.t',   # ok with 5.14, 5.18, 5.20, fails with 5.21
