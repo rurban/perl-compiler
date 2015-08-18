84d5a791ca59b53be5ea238383be0f3b67499890
Todd Rinaldo <toddr@cpan.org>
2015-08-17 23:06:15 -0500

    missing binop prototype

--- lib/B/C/Save/BINOP.pm
+++ lib/B/C/Save/BINOP.pm
@@ -1,14 +1,15 @@
 package B::BINOP;
 
-
+use B::C::File qw/binopsect/;
 
 sub save {
     my ( $op, $level ) = @_;
+    
     my $sym = B::objsym($op);
     return $sym if defined $sym;
 
-    $B::C::binopsect->comment("$opsect_common, first, last");
-    $B::C::binopsect->add(
+    binopsect->comment("$opsect_common, first, last");
+    binopsect->add(
         sprintf(
             "%s, s\\_%x, s\\_%x",
             $op->_save_common,
@@ -16,9 +17,9 @@ sub save {
             ${ $op->last }
         )
     );
-    $B::C::binopsect->debug( $op->name, $op->flagspv ) if $debug{flags};
-    my $ix = $B::C::binopsect->index;
-    $B::C::init->add( sprintf( "binop_list[$ix].op_ppaddr = %s;", $op->ppaddr ) )
+    binopsect->debug( $op->name, $op->flagspv );
+    my $ix = binopsect->index;
+    init->add( sprintf( "binop_list[$ix].op_ppaddr = %s;", $op->ppaddr ) )
       unless $B::C::optimize_ppaddr;
     $sym = B::C::savesym( $op, "(OP*)&binop_list[$ix]" );
     B::C::do_labels( $op, 'first', 'last' );
