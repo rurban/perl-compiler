5485d6408f6eaabfd0ac526c20b2f212b3988e56
Nicolas Rochelemagne <rochelemagne@cpanel.net>
2015-08-19 15:05:10 -0500

    NV

--- lib/B/C.pm
+++ lib/B/C.pm
@@ -915,33 +915,6 @@ sub nextcop {
     return ( $op and ref($op) eq 'B::COP' ) ? $op : undef;
 }
 
-sub B::NV::save {
-    my ( $sv, $fullname ) = @_;
-    my $sym = objsym($sv);
-    return $sym if defined $sym;
-    my $nv = nvx( $sv->NV );
-    $nv .= '.00' if $nv =~ /^-?\d+$/;
-
-    # IVX is invalid in B.xs and unused
-    my $iv = $sv->FLAGS & SVf_IOK ? $sv->IVX : 0;
-
-    xpvnvsect()->comment('STASH, MAGIC, cur, len, IVX, NVX');
-    xpvnvsect()->add( sprintf( "Nullhv, {0}, 0, 0, {%ld}, {%s}", $iv, $nv ) );
-
-    svsect()->add(
-        sprintf(
-            "&xpvnv_list[%d], %lu, 0x%x , {0}",
-            xpvnvsect()->index, $sv->REFCNT, $sv->FLAGS
-        )
-    );
-    svsect()->debug( $fullname, $sv );
-    warn sprintf(
-        "Saving NV %s to xpvnv_list[%d], sv_list[%d]\n",
-        $nv, xpvnvsect()->index, svsect()->index
-    ) if $debug{sv};
-    savesym( $sv, sprintf( "&sv_list[%d]", svsect()->index ) );
-}
-
 sub savepvn {
     my ( $dest, $pv, $sv, $cur ) = @_;
     my @init;
