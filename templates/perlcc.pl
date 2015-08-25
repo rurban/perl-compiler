e3ed352a345311a3ee978d12be78a25ea7c19fd5
Nicolas Rochelemagne <rochelemagne@cpanel.net>
2015-08-25 12:57:45 -0500

    Patch 0002 from Cpanel 514 B::C
    Change gcc calls from perlcc to not use -pipe to save
    memory
    
    Case 116089: This enhancement was found trying to fix failed build over memory usage

--- templates/perlcc.pl
+++ templates/perlcc.pl
@@ -619,6 +619,7 @@ sub cc_harness {
     my ( $cfile, $stash, $extra_libs ) = @_;
     use ExtUtils::Embed ();
     my $command = ExtUtils::Embed::ccopts . " -o $Output $cfile ";
+    $command =~ s/\s-pipe\s/ /;    # Strip -pipe out of the compile flags so less memory is used building binaries
     $command .= " -DHAVE_INDEPENDENT_COMALLOC" if $B::C::Flags::have_independent_comalloc;
     $command .= $B::C::Flags::extra_cflags if $B::C::Flags::extra_cflags;
     $command .= " -I" . $_ for split /\s+/, opt('I');
