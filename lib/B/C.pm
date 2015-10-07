rurban/perl-compiler

commit 1470b29d3b9bbfae0c9aaeac2a3a6af439607835

Remove the use of B::Section by adding its missing helper routines to B::C::Section
This also drops a use of base and strict. It also makes it so we don't have a dangling file handle on every compile

lib/B/C.pm
@@ -23,13 +23,15 @@ my $C99 = $Config{d_c99_variadic_macros}; # http://docs.sun.com/source/819-3688/
 package B::C::Section;
 use strict;
 
-use B ();
-use base 'B::Section'; # !!! get away with that. base is crazy and out of our control
+my %sections;
 
 sub new {
-  my $class = shift;
-  my $o     = $class->SUPER::new(@_);
+  my ($class, $section, $symtable, $default) = @_;
+  my $o = bless [-1, $section, $symtable, $default], $class;
+  $sections{$section} = $o;
+
   push @$o, { values => [] };
+
   # if sv add a dummy sv_arenaroot to support global destruction
   if ($_[0] eq 'sv') {
     $o->add( "0, 0, SVTYPEMASK|0x01000000".($] >= 5.009005?", {0}":'')); # SVf_FAKE
@@ -37,6 +39,12 @@ sub new {
   }
   return $o;
 }
+
+sub get {
+  my ($class, $section) = @_;
+  return $sections{$section};
+}
+
 
 sub add {
   my $section = shift;
@@ -51,6 +59,21 @@ sub remove {
 sub index {
   my $section = shift;
   return scalar( @{ $section->[-1]{values} } ) - 1;
+}
+
+sub name {
+  my $section = shift;
+  return $section->[1];
+}
+
+sub symtable {
+  my $section = shift;
+  return $section->[2];
+}
+
+sub default {
+  my $section = shift;
+  return $section->[3];
 }
 
 sub typename {
@@ -358,7 +381,7 @@ my %all_bc_subs = map {$_=>1}
 # uses now @B::C::Flags::deps
 our %all_bc_deps = map {$_=>1}
   @B::C::Flags::deps ? @B::C::Flags::deps
-  : qw(AnyDBM_File AutoLoader B B::AV B::Asmdata B::BINOP B::BM B::C B::C::Flags B::C::InitSection B::C::Section B::CC B::COP B::CV B::FAKEOP B::FM B::GV B::HE B::HV B::IO B::IV B::LEXWARN B::LISTOP B::LOGOP B::LOOP B::MAGIC B::NULL B::NV B::OBJECT B::OP B::PADLIST B::PADNAME B::PADNAMELIST B::PADOP B::PMOP B::PV B::PVIV B::PVLV B::PVMG B::PVNV B::PVOP B::REGEXP B::RHE B::RV B::SPECIAL B::STASHGV B::SV B::SVOP B::Section B::UNOP B::UV CORE CORE::GLOBAL Carp Config DB DynaLoader Errno Exporter Exporter::Heavy ExtUtils ExtUtils::Constant ExtUtils::Constant::ProxySubs Fcntl FileHandle IO IO::File IO::Handle IO::Poll IO::Seekable IO::Socket Internals O POSIX PerlIO PerlIO::Layer PerlIO::scalar Regexp SelectSaver Symbol UNIVERSAL XSLoader __ANON__ arybase arybase::mg base fields main maybe maybe::next mro next overload re strict threads utf8 vars version warnings warnings::register);
+  : qw(AnyDBM_File AutoLoader B B::AV B::Asmdata B::BINOP B::BM B::C B::C::Flags B::C::InitSection B::C::Section B::CC B::COP B::CV B::FAKEOP B::FM B::GV B::HE B::HV B::IO B::IV B::LEXWARN B::LISTOP B::LOGOP B::LOOP B::MAGIC B::NULL B::NV B::OBJECT B::OP B::PADLIST B::PADNAME B::PADNAMELIST B::PADOP B::PMOP B::PV B::PVIV B::PVLV B::PVMG B::PVNV B::PVOP B::REGEXP B::RHE B::RV B::SPECIAL B::STASHGV B::SV B::SVOP B::UNOP B::UV CORE CORE::GLOBAL Carp Config DB DynaLoader Errno Exporter Exporter::Heavy ExtUtils ExtUtils::Constant ExtUtils::Constant::ProxySubs Fcntl FileHandle IO IO::File IO::Handle IO::Poll IO::Seekable IO::Socket Internals O POSIX PerlIO PerlIO::Layer PerlIO::scalar Regexp SelectSaver Symbol UNIVERSAL XSLoader __ANON__ arybase arybase::mg base fields main maybe maybe::next mro next overload re strict threads utf8 vars version warnings warnings::register);
 
 # B::C stash footprint: mainly caused by blib, warnings, and Carp loaded with DynaLoader
 # perl5.15.7d-nt -MO=C,-o/dev/null -MO=Stash -e0
@@ -7531,7 +7554,7 @@ sub compile {
   $B::C::walkall  = 1;
 
   mark_skip qw(B::C B::C::Flags B::CC B::Asmdata B::FAKEOP O
-	       B::Section B::Pseudoreg B::Shadow B::C::InitSection);
+	       B::Pseudoreg B::Shadow B::C::InitSection);
   #mark_skip('DB', 'Term::ReadLine') if defined &DB::DB;
 
 OPTION:

lib/B/CC.pm
@@ -3472,7 +3472,7 @@ OPTION:
   #}
 
   mark_skip(qw(B::C B::C::Flags B::CC B::Asmdata B::FAKEOP
-               B::Section B::Pseudoreg B::Shadow B::C::InitSection
+               B::Pseudoreg B::Shadow B::C::InitSection
                O B::Stackobj B::Stackobj::Bool B::Stackobj::Padsv
                B::Stackobj::Const B::Stackobj::Aelem B::Bblock));
   $B::C::all_bc_deps{$_}++ for qw(Opcodes Opcode B::Concise attributes double int num str string subs);
@@ -3503,8 +3503,8 @@ sub compile {
   @options = import(@options);
 
   init_sections();
-  $init = B::Section->get("init");
-  $decl = B::Section->get("decl");
+  $init = B::C::Section->get("init");
+  $decl = B::C::Section->get("decl");
 
   # just some subs or main?
   if (@options) {

commit 99f2d65291ca51ac48cf3cd9c2282aba20355aad

C: fix B::C::Section - arena, global destruction
https://github.com/toddr/perl-compiler/commit/1470b29d3b9bbfae0c9aaeac2a3a6af439607835
broke the initialization of svsect.
We need sv_list[0] be the sv_arenaroot head.

lib/B/C.pm
@@ -33,7 +33,7 @@ sub new {
   push @$o, { values => [] };
 
   # if sv add a dummy sv_arenaroot to support global destruction
-  if ($_[0] eq 'sv') {
+  if ($section eq 'sv') {
     $o->add( "0, 0, SVTYPEMASK|0x01000000".($] >= 5.009005?", {0}":'')); # SVf_FAKE
     $o->[-1]{dbg}->[0] = "PL_sv_arenaroot";
   }
@@ -2724,6 +2724,7 @@ sub B::PV::save {
   my ( $savesym, $cur, $len, $pv, $static ) = save_pv_or_rv($sv, $fullname);
   $static = 0 if !($flags & SVf_ROK) and $sv->PV and $sv->PV =~ /::bootstrap$/;
   my $refcnt = $sv->REFCNT;
+  my $svix;
   # sv_free2 problem with !SvIMMORTAL and del_SV
   # repro with -O0 .. -O2 for all testcases
   if ($PERL518 and $fullname eq 'svop const') {
@@ -2740,31 +2741,33 @@ sub B::PV::save {
     $svsect->add( sprintf( "&xpv_list[%d], %Lu, 0x%x, {%s}",
                            $xpvsect->index, $refcnt, $flags,
 			   $savesym eq 'NULL' ? '0' :
-                             ($C99?".svu_pv=(char*)":"(char*)").$savesym ));
+                           ($C99?".svu_pv=(char*)":"(char*)").$savesym ));
+    $svix = $svsect->index;
     if ( defined($pv) and !$static ) {
       if ($shared_hek) {
         my $hek = save_hek($pv);
-        $init->add( sprintf( "sv_list[%d].sv_u.svu_pv = HEK_KEY(%s);", $svsect->index, $hek ))
+        $init->add( sprintf( "sv_list[%d].sv_u.svu_pv = HEK_KEY(%s);", $svix, $hek ))
                     unless $hek eq 'NULL';
       } else {
-        $init->add( savepvn( sprintf( "sv_list[%d].sv_u.svu_pv", $svsect->index ), $pv, $sv, $cur ) );
+        $init->add( savepvn( sprintf( "sv_list[%d].sv_u.svu_pv", $svix ), $pv, $sv, $cur ) );
       }
     }
     if ($debug{flags} and (!$ITHREADS or $]>=5.014) and $DEBUG_LEAKING_SCALARS) { # add sv_debug_file
       $init->add(sprintf(qq(sv_list[%d].sv_debug_file = %s" sv_list[%d] 0x%x";),
-			 $svsect->index, cstring($pv) eq '0' ? '"NULL"' : cstring($pv),
-			 $svsect->index, $sv->FLAGS));
+			 $svix, cstring($pv) eq '0' ? '"NULL"' : cstring($pv),
+			 $svix, $sv->FLAGS));
     }
   }
   else {
     $xpvsect->add( sprintf( "%s, %u, %u", "(char*)$savesym", $cur, $len ) );
     $svsect->add(sprintf("&xpv_list[%d], %Lu, 0x%x",
 			 $xpvsect->index, $refcnt, $flags));
+    $svix = $svsect->index;
     if ( defined($pv) and !$static ) {
       $init->add( savepvn( sprintf( "xpv_list[%d].xpv_pv", $xpvsect->index ), $pv, 0, $cur ) );
     }
   }
-  my $s = "sv_list[".$svsect->index."]";
+  my $s = "sv_list[$svix]";
   $svsect->debug( $fullname, $sv->flagspv ) if $debug{flags};
   savesym( $sv, "&".$s );
 }