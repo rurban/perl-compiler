bfc39307a33e1105792ff93b6fad2c52e38c41ee
Reini Urban <rurban@cpanel.net>
2015-10-22 10:16:53 +0200

    CORE: version-specific coretests as submodule
    See https://github.com/rurban/perl-compiler/issues/332
    Submodule stored at https://github.com/perl11/p5-coretests
    initialized with t/core-init.sh

---
+++ t/core-init.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+if [ ! -f Makefile.PL -a -f ../Makefile.PL ]; then
+    chdir ..
+fi
+if [ ! -f Makefile.PL ]; then
+    echo wrong basedir, missing Makefile.PL
+    exit
+fi
+if [ ! -d t/CORE ]; then
+    if [ ! -d .git ]; then
+        echo git clone p5-coretests.git t/CORE
+        git clone https://github.com/perl11/p5-coretests.git t/CORE
+    else
+        if [ ! -f .gitmodules ]; then
+            echo git submodule add p5-coretests.git t/CORE
+            git submodule add https://github.com/perl11/p5-coretests.git t/CORE
+        fi
+        echo git submodule update --remote
+        git submodule update --remote
+    fi
+else
+    echo git submodule update --remote
+    git submodule update --remote
+fi
--- t/coreall.sh
+++ t/coreall.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-t=${1:-t/CORE/comp/proto.t}
+# run all perls on a single core test
+t=${1:-t/CORE/v5.22/comp/proto.t}
 echo perlall='5.*-nt' perlall -m --nolog do $t
 perlall='5.*-nt' perlall -m --nolog do $t 2>&1 | egrep '(^not ok|/perl5.)'
