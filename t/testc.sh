6d04cf9549945e781bc621ba460b1780996c2f3d
Nicolas Rochelemagne <rochelemagne@cpanel.net>
2015-10-22 10:43:40 -0500

    Add options L and X to testc to show a test and create symlinks
    The symlink option is going to be used for parallel testing
    once the core test suite is merged.

--- t/testc.sh
+++ t/testc.sh
@@ -20,12 +20,14 @@ function help {
   echo "Without arguments try all $ntests tests. Without Option -Ox try -O0 to -O3 optimizations."
 }
 
-# use the actual perl from the Makefile (perl5.8.8, 
+# use the actual perl from the Makefile (perl5.8.8,
 # perl5.10.0d-nt, perl5.11.0, ...)
 PERL=`grep "^PERL =" Makefile|cut -c8-`
 PERL=${PERL:-perl}
 PERL=`echo $PERL|sed -e's,^",,; s,"$,,'`
 v518=`$PERL -e'print (($] < 5.018)?0:1)'`
+PERLV=$(perl -e 'print $^V')
+XTESTC="t/$PERLV/C-COMPILED/xtestc"
 
 function init {
 BASE=`basename $0`
@@ -35,9 +37,9 @@ Mblib=${Mblib:--Iblib/arch -Iblib/lib} # B::C is now fully 5.6+5.8 backwards com
 v513="`$PERL -e'print (($] < 5.013005) ? q() : q(-fno-fold,-fno-warnings,))'`"
 # OCMD=${OCMD}${v513}
 if [ -z "$Mblib" ]; then
-    VERS="${VERS}_global"; 
+    VERS="${VERS}_global";
     OCMD="$PERL $Mblib -MO=C,${v513}-Dcsp,"
-    if [ $BASE = "testcc.sh" ]; then # DrOsplt 
+    if [ $BASE = "testcc.sh" ]; then # DrOsplt
         OCMD="$PERL $Mblib -MO=CC,${v513}-DOsplt,"
     fi
 else
@@ -111,16 +113,47 @@ function runopt {
     fi
 }
 
+function emit_test {
+  n=$1
+  CONTENT="${tests[${n}]}"
+  if [ "x$CONTENT" != "x" ]; then
+    echo -E "$CONTENT"
+    echo
+    if [ "x${result[$n]}" = "x" ]; then result[$n]='ok'; fi
+    echo -E -n "### RESULT:${result[$n]}"
+  fi
+}
+
+function make_t_symlink {
+  [ -d "$XTESTC" ] || mkdir -p $XTESTC
+  n=$1
+  CONTENT="${tests[${n}]}"
+  if [ "x$CONTENT" != "x" ]; then
+    FILE_NUM=$(printf "%04d" $n)
+    FILE="$XTESTC/${FILE_NUM}.t"
+    ln -s  ../testc.pl $FILE
+  fi
+}
+
+function make_symlinks {
+  MAX=9999
+  rm -f $XTESTC/*.t ||:
+  for b in $(seq $MAX); do
+    make_t_symlink $b
+  done
+}
+
 function ctest {
     n=$1
     str=$2
-    if [ $BASE = "testcc.sh" ]; then 
+
+    if [ $BASE = "testcc.sh" ]; then
       o="cccode$n"
     else
       o="ccode$n"
     fi
     if [ -z "$str" ]; then
-        if [ "$n" = "08" ]; then n=8; fi 
+        if [ "$n" = "08" ]; then n=8; fi
         if [ "$n" = "09" ]; then n=9; fi
 	echo "${tests[${n}]}" > ${o}.pl
         str="${tests[${n}]}"
@@ -194,10 +227,10 @@ tests[8]='sub AUTOLOAD { print 1 } &{"a"}()'
 result[8]='1'
 tests[9]='my $l_i = 3; $x = sub { print $l_i }; &$x'
 result[9]='3'
-tests[10]='my $i_i = 1; 
+tests[10]='my $i_i = 1;
 my $foo = sub {
   $i_i = shift if @_
-}; print $i_i; 
+}; print $i_i;
 print &$foo(3),$i_i;'
 result[10]='133'
 # index: do fbm_compile or not
@@ -335,10 +368,10 @@ print "ok"'
 tests[71]='
 package my;
 our @a;
-sub f { 
+sub f {
   my($alias,$name)=@_;
   unshift(@a, $alias => $name);
-  my $find = "ok"; 
+  my $find = "ok";
   my $val = $a[1];
   if ( ref($alias) eq "Regexp" && $find =~ $alias ) {
     eval $val;
@@ -1143,7 +1176,7 @@ my $foo = sub {
 }->();
 $foo->method;'
 tests[350]='#TODO 5.18-5.22 dbg
-package Foo::Moose; use Moose; has bar => (is => "rw", isa => "Int"); 
+package Foo::Moose; use Moose; has bar => (is => "rw", isa => "Int");
 package main; my $moose = Foo::Moose->new; print "ok" if 32 == $moose->bar(32);'
 tests[368]='use EV; print q(ok)'
 tests[369]='
@@ -1174,10 +1207,12 @@ package main;
 my $f = foo->new( x => 5, y => 6);
 print $f->x . "\n";'
 result[371]='5'
+
 if [[ $v518 -gt 0 ]]; then
-  tests[372]='use utf8; require mro; my $f_gen = mro::get_pkg_gen("ᕘ"); undef %ᕘ::; mro::get_pkg_gen("ᕘ"); delete $::{"ᕘ::"}; print "ok";'
+  tests[372]='use utf8; require mro; my $f_gen = mro::get_pkg_gen('ᕘ'); undef %ᕘ::; mro::get_pkg_gen('ᕘ'); delete $::{"ᕘ::"}; print "ok";'
   tests[373]='package foo; BEGIN {undef %foo::} sub doof { caller(0) } print qq/ok\n/ if +(doof())[3] =~ qr/::doof/'
 fi
+
 tests[2050]='use utf8;package 텟ţ::ᴼ; sub ᴼ_or_Ḋ { "ok" } print ᴼ_or_Ḋ;'
 tests[2051]='use utf8;package ƂƂƂƂ; sub ƟK { "ok" } package ƦƦƦƦ; use base "ƂƂƂƂ"; my $x = bless {}, "ƦƦƦƦ"; print $x->ƟK();'
 tests[2052]='{ package Diӑmond_A; sub fಓ { "ok" } } { package Diӑmond_B; use base q{Diӑmond_A}; use mro "c3"; sub fಓ { (shift)->next::method() } } print Diӑmond_B->fಓ();'
@@ -1189,11 +1224,11 @@ tests[2055]='our %h; $h{""} = q/boom/; print qq{ok\n}'
  
 init
 
-# 
+#
 # getopts for -q -k -E -Du,-q -v -O2, -a -c -fro-inc
-while getopts "haAckoED:B:O:f:q" opt
+while getopts "XLhaAckoED:B:O:f:q" opt
 do
-  if [ "$opt" = "q" ]; then 
+  if [ "$opt" = "q" ]; then
     QUIET=1
     CCMD="$CCMD -q"
   fi
@@ -1205,7 +1240,7 @@ do
   # -D options: u,-q for quiet, no -D for verbose, -D- for no gcc warnings
   if [ "$opt" = "D" ]; then
     OCMD="$PERL $Mblib -MO=C,-D${OPTARG},"
-    if [ $BASE = "testcc.sh" ]; then 
+    if [ $BASE = "testcc.sh" ]; then
         OCMD="$PERL $Mblib -MO=CC,-D${OPTARG},"
     fi
     if [ -z "${OPTARG/-/}" ]; then
@@ -1213,7 +1248,7 @@ do
     fi
   fi
   # -B dynamic or -B static
-  if [ "$opt" = "B" ]; then 
+  if [ "$opt" = "B" ]; then
     CCMD="$CCMD -B${OPTARG}"
   fi
   if [ "$opt" = "O" ]; then OPTIM="$OPTARG"; fi
@@ -1221,10 +1256,19 @@ do
     OCMD="$(echo $OCMD|sed -e "s/C,/C,-f$OPTARG,/")"
   fi
   if [ "$opt" = "a" ]; then # replace -Du, by -Do
-    OCMD="$(echo $OCMD|sed -r -e 's/(-D.*)u,/\1o,/')" 
+    OCMD="$(echo $OCMD|sed -r -e 's/(-D.*)u,/\1o,/')"
   fi
   if [ "$opt" = "A" ]; then
       CCMD="$CCMD -DALLOW_PERL_OPTIONS"
+  fi
+  if [ "$opt" = "L" ]; then
+    make_symlinks
+    exit
+  fi
+  if [ "$opt" = "X" ]; then
+    shift
+    emit_test $1
+    exit
   fi
 done
 
@@ -1235,15 +1279,16 @@ if [ "$(perl -V:gccversion)" != "gccversion='';" ]; then
 	CCMD="$CCMD -g3"
     fi
 fi
+
 if [ -z $OPTIM ]; then OPTIM=-1; fi # all
 
 if [ -z "$QUIET" ]; then
-    make 
+    make
 else
     # O from 5.6 does not support -qq
     qq="`$PERL -e'print (($] < 5.007) ? q() : q(-qq,))'`"
-    # replace -D*,-v by -q 
-    OCMD="$(echo $OCMD    |sed -e 's/-D.*,//' -e 's/,-v,/,/' -e s/-MO=/-MO=$qq/)" 
+    # replace -D*,-v by -q
+    OCMD="$(echo $OCMD    |sed -e 's/-D.*,//' -e 's/,-v,/,/' -e s/-MO=/-MO=$qq/)"
     OCMDO1="$(echo $OCMDO1|sed -e 's/-D.*,//' -e 's/,-v,/,/' -e s/-MO=/-MO=$qq/)"
     OCMDO2="$(echo $OCMDO2|sed -e 's/-D.*,//' -e 's/,-v,/,/' -e s/-MO=/-MO=$qq/)"
     OCMDO3="$(echo $OCMDO3|sed -e 's/-D.*,//' -e 's/,-v,/,/' -e s/-MO=/-MO=$qq/)"
