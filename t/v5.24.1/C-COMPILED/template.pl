444220cf4491f9034e52837286439d72f72a698e
Nicolas Rochelemagne <rochelemagne@cpanel.net>
2015-11-10 09:47:52 -0600

    Make sure perl 5.22 is used when proving tests

--- t/v5.24.1/C-COMPILED/template.pl
+++ t/v5.24.1/C-COMPILED/template.pl
@@ -14,6 +14,8 @@ BEGIN {
     unshift @INC, $FindBin::Bin . "/../../../lib";
 }
 
+die "Please use perl 5.22" unless $^V =~ qr{^v5.22};
+
 # Used by runperl to find perlcc
 $ENV{'PROVE_BASEDIR'} = getcwd;
 
