ad2299a5808edf5ba14e896fbaf2d8bc669e6395
Reini Urban <rurban@cpanel.net>
2016-05-03 12:03:44 +0200

    comment about Google #364 $XS_VERSION

--- lib/B/C.pm
+++ lib/B/C.pm
@@ -7279,7 +7279,17 @@ _EOT9
               printf "\tmXPUSHp(\"%s\", %d);\n", $stashfile, length($stashfile);
             }
 	  }
-	  print "\tPUTBACK;\n";
+          # TODO Google #364: If a VERSION was provided need to add it here.
+          # Only fails with dev versions, like Socket 2.021_01 now.
+          # The best fix is to make XS_VERSION visible, via use vars (global),
+          # or our (in stash), but not via my. Fixing it up in the compiler is too hard,
+          # We would need to search for XS_VERSION in the main_cv padnames, check
+          # for _ and push it.
+          # E.g.
+          #   our $VERSION = '2.021_01';
+          #   our $XS_VERSION = $VERSION;   # A dev xs version needs to be global, not my
+          #   $VERSION = eval $VERSION;
+          print "\tPUTBACK;\n";
 	  warn "bootstrapping $stashname added to XSLoader dl_init\n" if $verbose;
 	  # XSLoader has the 2nd insanest API in whole Perl, right after make_warnings_object()
 	  # 5.15.3 workaround for [perl #101336]
