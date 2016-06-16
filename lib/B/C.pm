7eb592883f245be4a675ed4dde8558340acf2cc6
Nicolas Rochelemagne <rochelemagne@cpanel.net>
2016-06-16 09:40:48 -0600

    Fix to compile in utf8_heavy.pl correctly
    GH #369: Subroutines redefined from utf8_heavy
    
    utf8_heavy.pl could be compiled in the binary
    without setting INC correctly. Provide a common
    helper to compile in utf8_heavy.
    
    References: CPANEL-5417, CPANEL-6105

--- lib/B/C.pm
+++ lib/B/C.pm
@@ -1476,9 +1476,8 @@ sub B::OP::save {
     if ($] >= 5.013009) {
       warn "enabling -ffold with ucfirst\n" if $verbose;
       require "utf8.pm" unless $savINC{"utf8.pm"};
-      require "utf8_heavy.pl" unless $savINC{"utf8_heavy.pl"}; # bypass AUTOLOAD
       mark_package("utf8");
-      mark_package("utf8_heavy.pl");
+      load_utf8_heavy();
     }
   }
   if (ref($op) eq 'B::OP') { # check wrong BASEOPs
@@ -2604,8 +2603,7 @@ sub B::PMOP::save {
       # Since 5.13.10 with PMf_FOLD (i) we need to swash_init("utf8::Cased").
       if ($] >= 5.013009 and $pmflags & 4) {
         # Note: in CORE utf8::SWASHNEW is demand-loaded from utf8 with Perl_load_module()
-        require "utf8_heavy.pl" unless $savINC{"utf8_heavy.pl"}; # bypass AUTOLOAD
-        svref_2object( \&{"utf8\::SWASHNEW"} )->save; # for swash_init(), defined in lib/utf8_heavy.pl
+        load_utf8_heavy();
         if ($PERL518 and !$swash_init and $swash_ToCf) {
           $init->add("PL_utf8_tofold = $swash_ToCf;");
           $swash_init++;
@@ -3910,6 +3908,25 @@ sub try_isa {
   return 0; # not found
 }
 
+sub load_utf8_heavy {
+    return if $savINC{"utf8_heavy.pl"};
+
+    require 'utf8_heavy.pl';
+    mark_package('utf8_heavy.pl');
+    $curINC{'utf8_heavy.pl'} = $INC{'utf8_heavy.pl'};
+    $savINC{"utf8_heavy.pl"} = 1;
+    add_hashINC("utf8");
+
+    # FIXME: we want to use add_hashINC for utf8_heavy, inc_packname should return an array
+    # add_hashINC("utf8_heavy.pl");
+
+    # In CORE utf8::SWASHNEW is demand-loaded from utf8 with Perl_load_module()
+    # It adds about 1.6MB exe size 32-bit.
+    svref_2object( \&{"utf8\::SWASHNEW"} )->save;
+
+    return 1;
+}
+
 # If the sub or method is not found:
 # 1. try @ISA, mark_package and return.
 # 2. try UNIVERSAL::method
@@ -3930,7 +3947,8 @@ sub try_autoload {
 		$fullname, $cvstashname ) if $debug{cv};
   if ($fullname eq 'utf8::SWASHNEW') {
     # utf8_heavy was loaded so far, so defer to a demand-loading stub
-    my $stub = sub { require 'utf8_heavy.pl' unless $savINC{"utf8_heavy.pl"}; goto &utf8::SWASHNEW; };
+    # always require utf8_heavy, do not care if it s already in
+    my $stub = sub { require 'utf8_heavy.pl'; goto &utf8::SWASHNEW };
     return svref_2object( $stub );
   }
 
@@ -4220,10 +4238,7 @@ sub B::CV::save {
     push_package($package_pv);
   }
   if ($fullname eq 'utf8::SWASHNEW') { # bypass utf8::AUTOLOAD, a new 5.13.9 mess
-    require "utf8_heavy.pl" unless $savINC{"utf8_heavy.pl"};
-    # sub utf8::AUTOLOAD {}; # How to ignore &utf8::AUTOLOAD with Carp? The symbol table is
-    # already polluted. See issue 61 and force_heavy()
-    svref_2object( \&{"utf8\::SWASHNEW"} )->save;
+    load_utf8_heavy();
   }
 
   if ($fullname eq 'IO::Socket::SSL::SSL_Context::new') {
@@ -7744,6 +7759,11 @@ sub walksymtable {
   my ($sym, $ref, $fullname);
   $prefix = '' unless defined $prefix;
 
+# If load_utf8_heavy doesn't happen before we walk utf8::
+# (when utf8_heavy has already been called) then the stored CV for utf8::S
+# WASHNEW could be wrong.
+  load_utf8_heavy() if ( $prefix eq 'utf8::' && defined $symref->{'SWASHNEW'} );
+
   my @list = sort {
     # we want these symbols to be saved last to avoid incomplete saves
     # +/- reverse is to defer + - to fix Tie::Hash::NamedCapturespecial cases. GH #247
@@ -8202,12 +8222,7 @@ sub save_unused_subs {
       or ($savINC{'utf8_heavy.pl'} and ($B::C::fold or exists($savINC{'utf8.pm'})))) {
     require "utf8.pm" unless $savINC{"utf8.pm"};
     mark_package('utf8');
-    require "utf8_heavy.pl" unless $savINC{"utf8_heavy.pl"}; # bypass AUTOLOAD
-    mark_package('utf8_heavy.pl');
-    # In CORE utf8::SWASHNEW is demand-loaded from utf8 with Perl_load_module()
-    # It adds about 1.6MB exe size 32-bit.
-    svref_2object( \&{"utf8\::SWASHNEW"} )->save;
-    add_hashINC("utf8");
+    load_utf8_heavy();
   }
   # run-time Carp
   # With -fno-warnings we don't insist on initializing warnings::register_categories and Carp.
