656a828f39ff7749961a9dbbe3fc12299ddc6522
Todd Rinaldo <toddr@cpanel.net>
2016-09-21 13:30:58 -0500

    No longer treat strings with only 0 in them as a special string

--- lib/B/C/SaveCOW.pm
+++ lib/B/C/SaveCOW.pm
@@ -33,16 +33,15 @@ sub savepv {
     
     return @{$strtable{$cstring}} if defined $strtable{$cstring};
     my $pvsym = sprintf( "cowpv%d", inc_pv_index() );
+    print STDERR "$pvsym == $cstring\n";
     if ( defined $max_string_len && $cur > $max_string_len ) {
         my $chars = join ', ', map { cchar $_ } split //, pack( "a*", $pv );
         decl()->add( sprintf( "Static const char %s[] = { %s };", $pvsym, $chars ) );
         $strtable{$cstring} = [$pvsym, $cur, $len];
     }
     else {
-        if ( $pv ne "0" ) {    # sic
-            decl()->add( sprintf( "Static const char %s[] = %s;", $pvsym, $cstring ) );
-            $strtable{$cstring} = [$pvsym, $cur, $len];
-        }
+        decl()->add( sprintf( "Static const char %s[] = %s;", $pvsym, $cstring ) );
+        $strtable{$cstring} = [$pvsym, $cur, $len];
     }
     return ($pvsym, $cur, $len); # NOTE: $cur is total size of the perl string. len would be the length of the C string.
 }
