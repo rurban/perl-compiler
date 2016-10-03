11340a7389c7a073bb698b5882ab21e02a5bf800
Todd Rinaldo <toddr@cpanel.net>
2016-10-03 11:37:13 -0500

    Broken on this branch

--- test.pl
+++ test.pl
@@ -1,22 +1,10 @@
 #!/bin/env perl
-my $p;
-BEGIN {
-	$p = {
-		a => 1,
-		b => 2,
-		c => 3,
-	};
-	my @a = ( 1..1000 );
-	$p = { %$p, @a };
 
-}
+use JSON::PP ();
+
+my $perl_hash_or_arrayref =  { foo => 1 };
 
 
+print JSON::PP::encode_json( $perl_hash_or_arrayref);
 
-print ref($p) . "\n";
-
-print "a => $p->{a}\n";
-print "b => $p->{b}\n";
-print "c => $p->{c}\n";
-print scalar(%$p) . "\n";
-print join( " - ", keys(%$p), "\n");
+print "\n\n";
